d087212994c4fbfd87a8552e208781e342996046
Rachel <rbennett1984@gmail.com>
2019-05-16 14:30:30 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// With JS in strict mode, you can not use undeclared variables.//
+'use strict';
+// create array for existing items on list
+const STORE = [
+    {name: "Whole Grains", checked: false},
+    {name: "Leafy Greens", checked: false},
+    {name: "Blueberries", checked: true},
+    {name: "Turmeric", checked: false},
+    {name: "Green Tea", checked: false},
+    {name: "Nuts and Seeds", checked: true},
+    {name: "Soy", checked: false},
+    {name: "Shiitake Mushrooms", checked: false},
+    {name: "Garlic", checked: true}
+];
+
+
+function generateItemElement(item, itemIndex, template) {
+  return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+}
+
+//The join() method joins the elements of an array into a string, and returns the string.
+function generateShoppingItemsString(shoppingList) {
+  console.log("Generating shopping list elements");
+
+  const items = shoppingList.map((item, index) => generateItemElement(item, index));
+  return items.join("");
+}
+
+//render list AND INSERT html to DOM 
+function renderShoppingList() {
+
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(STORE);
+$('.js-shopping-list').html(shoppingListItemsString);
+}
+
+
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({name: itemName, checked: false});
+}
+
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+
+function toggleCheckedForListItem(itemIndex) {
+  console.log("Toggling checked property for item at index " + itemIndex);
+  STORE[itemIndex].checked = !STORE[itemIndex].checked;
+}
+
+
+function getItemIndexFromElement(item) {
+  const itemIndexString = $(item)
+    .closest('.js-item-index-element')
+    .attr('data-item-index');
+  return parseInt(itemIndexString, 10);
+}
+//to check-off items
+function handleItemCheckClicked() {
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+    toggleCheckedForListItem(itemIndex);
+    renderShoppingList();
+  });
+}
+
+// to delete items from list
+function deleteListItem(itemIndex) {
+  console.log(`Deleting item at index  ${itemIndex} from shopping list`)
+
+  STORE.splice(itemIndex, 1);
+}
+
+ // Event delegation allows us to attach a single event listener, to a parent element, that will fire for all descendants matching a selector, whether those descendants exist now or are added in the future.
+function handleDeleteItemClicked() {
+
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    const itemIndex = getItemIndexFromElement(event.currentTarget);
+  
+    deleteListItem(itemIndex);
+  
+    renderShoppingList();
+  });
+}
+//callbacks
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+// callback when the page loads
+$(handleShoppingList);
